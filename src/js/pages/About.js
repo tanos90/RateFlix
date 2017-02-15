1a04bde0c064ff2cbaa7b637d2aa77dc6b47e243
Juan Carlos Navarro Burgos <juan navarro@tiempodev.local>
2017-02-15 11:13:43 -0600

    Initial Commit, basic front end structure

---
+++ src/js/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default class About extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div class="well">
+                <h4>About</h4>
+            </div>
+        );
+    }
+};
